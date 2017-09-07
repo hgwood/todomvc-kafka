9f9d4050c597c1ddf233748aa9b92cfc7687f635
Hugo Wood <hx4.5@free.fr>
2017-09-08 00:11:46 +0200

    :hammer: replace docker kafka by embedded kafka
    Works!

--- src/main/java/fr/hgwood/todomvckafka/App.java
+++ src/main/java/fr/hgwood/todomvckafka/App.java
@@ -1,13 +1,32 @@
 package fr.hgwood.todomvckafka;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.util.Arrays;
+import java.util.Properties;
+
 /**
  * Hello world!
- *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static final String INPUT_TOPIC = "input-topic";
+    public static final String OUTPUT_TOPIC = "output-topic";
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "application-id");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-broker1:9092");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        KStreamBuilder builder = new KStreamBuilder();
+        builder.stream(INPUT_TOPIC).to(OUTPUT_TOPIC);
+
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
     }
 }
--- src/test/java/fr/hgwood/todomvckafka/AppTest.java
+++ src/test/java/fr/hgwood/todomvckafka/AppTest.java
@@ -1,27 +1,51 @@
 package fr.hgwood.todomvckafka;
 
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-import java.io.File;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
 
-    public static final int ZOOKEEPER_PORT = 2181;
-    public static final int KAFKA_PORT = 9092;
+    private static final String TEMPLATE_TOPIC = "template-topic";
 
-    @ClassRule public static final DockerComposeContainer dockerComposeContainer =
-        new DockerComposeContainer(
-            new File("src/test/resources/compose-test.yml"))
-            .withLocalCompose(true)
-            .withExposedService("zookeeper_1", ZOOKEEPER_PORT)
-            .withExposedService("kafka_1", KAFKA_PORT);
+    private static final Serde<String> stringSerde = Serdes.String();
+
+    @ClassRule
+    public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, TEMPLATE_TOPIC);
+
 
     @Test
-    public void dockerComposeContainerStarts() {
+    public void testHarnessStarts() throws Exception {
+        Map<String, Object> producerProps =
+            KafkaTestUtils.producerProps(embeddedKafka);
+        ProducerFactory<String, String> producerFactory =
+            new DefaultKafkaProducerFactory<>(producerProps, stringSerde.serializer(), stringSerde.serializer());
+        KafkaTemplate<String, String> template = new KafkaTemplate<>(producerFactory);
 
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps(
+            "testGroup", "true", embeddedKafka);
+        consumerProps.put("auto.offset.reset", "earliest");
+        ConsumerFactory consumerFactory = new DefaultKafkaConsumerFactory(
+            consumerProps, stringSerde.deserializer(), stringSerde.deserializer());
+        Consumer<String, String> consumer = consumerFactory.createConsumer();
+        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, TEMPLATE_TOPIC);
+
+        template.send(TEMPLATE_TOPIC, "foo");
+
+        ConsumerRecord<String, String> reply = KafkaTestUtils.getSingleRecord(
+            consumer, TEMPLATE_TOPIC);
+        assertEquals("foo", reply.value());
     }
 
 }
