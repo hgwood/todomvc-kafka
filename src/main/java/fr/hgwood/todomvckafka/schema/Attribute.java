934766985d90ed047aef7525118678bb8a883fbc
Hugo Wood <hx4.5@free.fr>
2017-09-21 17:58:54 +0200

    :memo: Attribute javadoc

--- src/main/java/fr/hgwood/todomvckafka/schema/Attribute.java
+++ src/main/java/fr/hgwood/todomvckafka/schema/Attribute.java
@@ -2,6 +2,17 @@ package fr.hgwood.todomvckafka.schema;
 
 import lombok.Value;
 
+/**
+ * This class makes up the schema of the entities stored by the system. It is inspired by Datomic.
+ * Attribute associate a name (which should be unique) to a type. Attributes can be attached to any
+ * entity through a {@link fr.hgwood.todomvckafka.facts.Fact}.
+ *
+ *
+ * This should be an enum but enums don't support type parameters. So it falls back to constant
+ * instances as static fields.
+ *
+ * @param <T> the type of the attribute's value.
+ */
 @Value
 public class Attribute<T> {
     public static Attribute<String> TODO_ITEM_TEXT =
